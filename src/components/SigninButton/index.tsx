27de74d36cb341ade82effdb6c2dd2ec0d593b54
DanyLass <danidias.schilling@gmail.com>
2022-02-14 19:20:39 -0300

    creation of auth folder and nextauth file

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn } from "next-auth/client";
 export function SignInButton() {
   const isUserLoggedIn = true;
   return isUserLoggedIn ? (
@@ -10,7 +11,11 @@ export function SignInButton() {
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
