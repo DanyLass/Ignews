01bf6e0ebd485dc55251022e167c2dd014528b92
DanyLass <danidias.schilling@gmail.com>
2022-02-16 20:56:51 -0300

    functionality has been given to the signinButton

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,14 +1,21 @@
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Daniela Dias
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
