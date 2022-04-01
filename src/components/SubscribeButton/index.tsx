1cf3eaeeceeac8e8cfb96c734cfaffc9477e5980
DanyLass <danidias.schilling@gmail.com>
2022-04-01 17:25:24 -0300

    listing posts in the screen

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,8 @@
 import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
+import { stripe } from "../../services/stripe";
+import { getStripeJs } from "../../services/stripe-js";
 
 interface SubscribeButtonProps {
   priceId: string;
@@ -8,10 +11,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const { data: session } = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
+    }
+
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId: sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
   return (
